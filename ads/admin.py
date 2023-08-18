9a30e26ab6ceeeda21d5604371a9963930236964
Kairat <kajratimankulov14@gmail.com>
2023-08-18 19:01:33 +0600

    created User, Category, Subcategory models

--- ads/admin.py
+++ ads/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Ads
+from .models import Ads, Category, Subcategory
 
 
+admin.site.register(Category)
+admin.site.register(Subcategory)
 admin.site.register(Ads)
--- ads/views.py
+++ ads/views.py
@@ -5,11 +5,7 @@ from .models import Ads
 
 def ads_list (request):
     all_ads = Ads.objects.all()
-    first = all_ads[0]
-    print(type(all_ads))
-    print(first.title)
-    print(first.description)
-    print(first.price)
+    
     context = {
         "all_ads": all_ads
     }
